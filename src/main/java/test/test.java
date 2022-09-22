bed6538471ca422f32b730e1519309bc2a14697e
Erickin <956968575@qq.com>
2022-09-22 20:38:11 +0800

    Initial commit

---
+++ src/main/java/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @BelongsProject: test01
+ * @BelongsPackage: test
+ * @Author: Erickin
+ * @Date: 2022/9/22 20:33
+ * @Description: TODO
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
